dd02f6162fb0548efe5e360c5a15e84e8e079902
ddg999 <slowman918@gmail.com>
2024-08-07 11:18:07 +0900

    로그인,로그아웃 기능구현

--- src/main/java/com/tenco/bank/dto/SignInDTO.java
+++ src/main/java/com/tenco/bank/dto/SignInDTO.java
@@ -13,15 +13,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @Builder
 @ToString
-public class SignUpDTO {
+public class SignInDTO {
 	private String username;
 	private String password;
-	private String fullname;
 
-	// 2단계 로직 - User Object 반환
 	public User toUser() {
-		return User.builder().username(this.username).password(this.password).fullname(this.fullname).build();
+		return User.builder().username(this.username).password(this.password).build();
 	}
-
-	// TODO 추후 사진 업로드 기능 추가 예정
 }
